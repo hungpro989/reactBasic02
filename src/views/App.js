ad3da980845d0d44c0725db8dd120d567696ece7
hungpro989 <hungpro989.hp95@gmail.com>
2022-04-21 02:33:54 +0700

    2 commit

--- src/views/App.js
+++ src/views/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.scss';
+import ExampleComponent from './Example/ExampleComponent';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <ExampleComponent />
       </header>
     </div>
   );
---
+++ src/views/Example/ExampleComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class ExampleComponent extends React.Component {
+    render() {
+        return (
+            <div>Hưng chỉ yêu chang</div>
+        )
+    }
+}
+export default ExampleComponent;
