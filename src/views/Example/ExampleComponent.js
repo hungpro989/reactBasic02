2a9aad8cf5d269cb67db12f3c64b78f505414335
hungpro989 <hungpro989.hp95@gmail.com>
2022-04-21 03:11:15 +0700

    3 commit

--- src/views/Example/ExampleComponent.js
+++ src/views/Example/ExampleComponent.js
@@ -1,9 +1,27 @@
 import React from "react";
 
+
+
 class ExampleComponent extends React.Component {
+    // state là: hành động reload các component
+    //<>todo</> là: cách render 2 hay nhiều div trong 1 component
+    state = {
+        //key:'value'
+        name: 'Hưng',
+        desc: 'Người đẹp zai nhất'
+    }
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
         return (
-            <div>Hưng chỉ yêu chang</div>
+            <div>
+                <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+                <h1>Xin chào, Mr.{this.state.name}</h1>
+                <h2>Mô tả: {this.state.desc}</h2>
+            </div>
         )
     }
 }
